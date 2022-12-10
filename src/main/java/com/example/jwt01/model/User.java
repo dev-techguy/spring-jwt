dccbd0129dbbe8e46c6eef6c1653c0c075254b6e
dev-techguy <dev.techguy@gmail.com>
2022-12-10 12:40:52 +0300

    :ok_hand: Updating code due to code on authorization and authentication.

--- src/main/java/com/example/jwt01/model/User.java
+++ src/main/java/com/example/jwt01/model/User.java
@@ -1,10 +1,10 @@
 package com.example.jwt01.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
